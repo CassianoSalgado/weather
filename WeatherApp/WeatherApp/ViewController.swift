a95938a3491d475509c59ea8af16f35e0dfe4313
CassianoSalgado <cassiano.salgado01@gmail.com>
2022-06-24 13:48:33 -0300

    initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Cassiano Carradore Salgado on 24/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
