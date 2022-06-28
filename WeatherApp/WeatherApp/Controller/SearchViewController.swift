6f608a4a8c69270e3ab2b49ac0433748070c16cd
CassianoSalgado <cassiano.salgado01@gmail.com>
2022-06-28 09:19:55 -0300

    first step to clean code: deleted some comments

--- WeatherApp/WeatherApp/Controller/SearchViewController.swift
+++ WeatherApp/WeatherApp/Controller/SearchViewController.swift
@@ -141,19 +141,9 @@ class SearchViewController: UIViewController, MKLocalSearchCompleterDelegate, UI
             if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailsViewController") as? DetailsViewController {
                         vc.selectedCity = searchResults[indexPath.row].title
                 self.navigationController?.pushViewController(vc, animated: true)
-                    }
+            }
         }
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
